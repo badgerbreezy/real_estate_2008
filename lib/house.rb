48f284bd488c5ec8dd2be2f025590ad5c3ce4a2a
Brian Liu <brian.b.liu@gmail.com>
2020-08-26 09:22:33 -0600

    Add attr_reader and instance variables.

--- lib/house.rb
+++ lib/house.rb
@@ -1,4 +1,11 @@
 class House
+  attr_reader :room
+              :price
+              :address
 
-
+  def initialize(price, address)
+    @price   = price
+    @address = address
+    @rooms   = []
+  end
 end
--- lib/room.rb
+++ lib/room.rb
@@ -1,23 +1,21 @@
- class Room
-attr_reader :category,
-            :area
+class Room
+  attr_reader :category,
+              :area
+  def initialize(category, length, width)
+    @category = category
+    @area     = length * width.to_i
+    @paint    = nil
+  end
 
-   def initialize(category, length, width)
-     @category = category
-     @area = length * width.to_i
-     @paint = nil
-   end
+  def is_painted?
+    if @paint == nil
+      false
+    else
+      true
+    end
+  end
 
-   def is_painted?
-     if @paint == nil
-       false
-     else
-       true
-     end
-   end
-
-   def paint
-     @paint = true
-   end
-
- end
+  def paint
+    @paint = true
+  end
+end
