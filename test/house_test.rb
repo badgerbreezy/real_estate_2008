1f5e0c1b43c09a27c85a3899d3d91fd6c173fb02
Brian Liu <brian.b.liu@gmail.com>
2020-08-26 09:19:06 -0600

    Create house class and house test.

---
+++ test/house_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+require './lib/house'
+
+class HouseTest < Minitest::Test
+  def test_it_exists
+    house = House.new
+    assert_instance_of House, house
+  end
+end
